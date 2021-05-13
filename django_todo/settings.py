18c1eb2c0f9f25435c2b8caf37116f44da114b50
thaysom22 <thaysom22@gmail.com>
2021-05-13 13:14:46 +0000

    create SECRET_KEY environment variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^a5jgaht&7xhmrpp00nu)!!0ozii_w1+(vm+*8681ypcb+63uj')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production (this could expose internal source code to users)
 DEBUG = development
--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item  # .models b/c from current dir's models file
+from .models import Item  # .models (. b/c from current dir's models file)
 
 # Register your models here.
 admin.site.register(Item)
