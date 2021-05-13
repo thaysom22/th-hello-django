513f495410182f90bb275117837c473dfeb7b1ac
thaysom22 <thaysom22@gmail.com>
2021-05-13 04:35:36 +0000

    written tests with 100% coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
-class TestDjango(TestCase):
 
-    def test_thing_works(self):
-        self.assertEqual(1, 1)
+class TestModels(TestCase):
 
+    # item.done defaults to False when Item instance is created
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
